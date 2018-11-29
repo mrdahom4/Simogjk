141a4bec4d13db643b7edb6b261359cfeb5255c5
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-11-29 23:00:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -458,20 +458,6 @@ client.on('message', (message) => {
 }); 
 
 
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-  wait(1000);
-
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
